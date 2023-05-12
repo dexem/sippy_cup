95da997c3f7e9fc4f5e0ac44b320632a60b27be4
Eric Ravelo <eravelo@dexem.com>
2023-05-12 18:27:59 +0200

    Remove all [next_url] (breaking with jambonz)

--- lib/sippy_cup/scenario.rb
+++ lib/sippy_cup/scenario.rb
@@ -446,7 +446,7 @@ a=rtpmap:0 PCMU/8000
     def ack_answer(opts = {})
       msg = <<-BODY
 
-ACK [next_url] SIP/2.0
+ACK SIP/2.0
 Via: SIP/2.0/[transport] #{@adv_ip}:[local_port];branch=[branch]
 From: "#{@from_user}" <sip:#{@from_user}@#{@adv_ip}:[local_port]>;tag=[call_number]
 To: <sip:#{to_addr}>[peer_tag_param]
@@ -497,7 +497,7 @@ Content-Length: 0
         when :info
           info = <<-INFO
 
-INFO [next_url] SIP/2.0
+INFO SIP/2.0
 Via: SIP/2.0/[transport] #{@adv_ip}:[local_port];branch=[branch]
 From: "#{@from_user}" <sip:#{@from_user}@#{@adv_ip}:[local_port]>;tag=[call_number]
 To: <sip:#{to_addr}>[peer_tag_param]
@@ -561,7 +561,7 @@ Duration=#{delay}
     def send_bye(opts = {})
       msg = <<-MSG
 
-BYE [next_url] SIP/2.0
+BYE SIP/2.0
 Via: SIP/2.0/[transport] #{@adv_ip}:[local_port];branch=[branch]
 From: <sip:[$local_addr]>;tag=[call_number]
 To: <sip:[$remote_addr]>;tag=[$remote_tag]
